8f4d247ed95b9eec825ad5a0780b227e07f42acf
roman-glushkov <roma.glushkov.vuz@mail.ru>
2025-02-17 09:24:19 +0300

    modified:   LAB 1/me.html 	new file:   LAB 2/4.jpg 	new file:   LAB 2/fon2.jpg 	modified:   LAB 2/home.php 	new file:   "\320\241\320\262\320\276\320\265/static/images/fon2.jpg"

--- LAB 2/home.php
+++ LAB 2/home.php
@@ -1,32 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
-<style>
-    body {
-        background-image: url('fon2.jpg'); 
-        background-size: cover; 
-        background-position: center; 
-        background-repeat: no-repeat; 
-    }
-    .left { text-align: left; }   
-    .center { text-align: center; }
-    .right { text-align: right; }   
-    .img-left { display: block; margin: 0 auto 0 0; }
-    .img-center { display: block; margin: 0 auto; } 
-    .img-right { display: block; margin: 0 0 0 auto; } 
-</style>      
 <head>
-    <title> Дом </title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
 </head>
 <body>
-    <h1 class="center"> Глушков Роман </h1>
-    <img 
-        src="4.jpg" 
-        alt="Роман Глушков" 
-        width="500px "
-        class="img-center">
-    <h3 class="left"> ВУЗ: Волгатех </h3>
-    <h3 class="left"> Группа: ПС - 12 </h3>
-    <h3 class="left"> О себе</h3>
-    <h6 class="left"> Волейболист </h6>
+    
 </body>
-</html>  
+</html>
