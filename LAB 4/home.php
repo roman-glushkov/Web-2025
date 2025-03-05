72c75b735ee9b23bc4478cdda31dce84be898ebe
roman-glushkov <roma.glushkov.vuz@mail.ru>
2025-03-05 09:09:40 +0300

    new file:   LAB 2/home.php 	new file:   LAB 2/post.php 	new file:   LAB 3/QUERY_STRING.exe 	new file:   LAB 3/QUERY_STRING.pas 	new file:   LAB 4/home.php 	new file:   LAB 4/login.php 	new file:   LAB 4/profile.php 	new file:   LAB 4/static/images/avatar.jpg 	new file:   LAB 4/static/images/book.png 	new file:   LAB 4/static/images/cupcake.png 	new file:   LAB 4/static/images/flowers.png 	new file:   LAB 4/static/images/girl.png 	new file:   LAB 4/static/images/heart.png 	new file:   LAB 4/static/images/house.png 	new file:   LAB 4/static/images/houses.png 	new file:   LAB 4/static/images/image.png 	new file:   LAB 4/static/images/men.jpg 	new file:   LAB 4/static/images/number_post.png 	new file:   LAB 4/static/images/oko.png 	new file:   LAB 4/static/images/park.jpg 	new file:   LAB 4/static/images/party.png 	new file:   LAB 4/static/images/pensil.png 	new file:   LAB 4/static/images/plus.png 	new file:   LAB 4/static/images/pot.jpg 	new file:   LAB 4/static/images/profile.png 	new file:   LAB 4/static/images/rocets.png 	new file:   LAB 4/static/images/street.png 	new file:   LAB 4/static/images/two.jpg 	new file:   LAB 4/static/images/women.jpg 	new file:   LAB 4/style_login.css 	new file:   "LAB 4/\320\273\320\265\320\272\321\206\320\270\321\217.html"

---
+++ LAB 4/home.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>News</title>
+  </head>
+  <body>
+    <table>
+      <tr>
+        <td>
+          <table>
+            <tr>
+              <td>
+                <img
+                  src="static/images/house.png"
+                  alt="house"
+                  width="40"
+                  height="40"
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <img
+                  src="static/images/profile.png"
+                  alt="profile"
+                  width="40"
+                  height="40"
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <img
+                  src="static/images/plus.png"
+                  alt="plus"
+                  width="40"
+                  height="40"
+                />
+              </td>
+            </tr>
+          </table>
+        </td>
+        <td>
+          <div class="pages">
+            <table>
+              <tr>
+                <td>
+                  <img
+                    src="static/images/avatar.jpg"
+                    alt="avatar"
+                    width="32"
+                    height="32"
+                  />
+                </td>
+                <td>Ваня Денисов</td>
+                <td>
+                  <img
+                    src="static/images/pensil.png"
+                    alt="pensil"
+                    width="24"
+                    height="24"
+                  />
+                </td>
+              </tr>
+            </table>
+            <img
+              src="static/images/street.png"
+              alt="street"
+              width="474"
+              height="474"
+            />
+            <table>
+              <tr>
+                <td><img src="static/images/heart.png" alt="heart" /></td>
+                <td>203</td>
+              </tr>
+            </table>
+            <p>Так красиво сегодня на улице! Настоящая зима)) Вспоминается</p>
+            <p>
+              Бродский: «Поздно ночью, в уснувшей долине, на самом дне, в
+              гор...»
+            </p>
+            <p>ещё</p>
+            <p>2 часа назад</p>
+            <table>
+              <tr>
+                <td>
+                  <img
+                    src="static/images/girl.png"
+                    alt="avatar"
+                    width="32"
+                    height="32"
+                  />
+                </td>
+                <td>Лиза Дёмина</td>
+              </tr>
+            </table>
+            <img
+              src="static/images/flowers.png"
+              alt="street"
+              width="474"
+              height="474"
+            />
+          </div>
+        </td>
+      </tr>
+    </table>
+  </body>
+</html>
